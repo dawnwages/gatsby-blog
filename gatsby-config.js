ebe84d15f9a26382f59ca81ace24c88640b1e670
Dawn <dawn.wages@gmail.com>
2019-12-20 22:10:29 +0000

    gatsby google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,33 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-29082978-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "UA-29082978-1",
+        // Enables Google Optimize Experiment ID
+        experimentId: "UA-29082978-1",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "UA-29082978-1",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "https://glittertech.dev",
+      },
+    },
+    {
       resolve: `gatsby-plugin-netlify-cms`,
       options: {
         modulePath: `${__dirname}/static/cms/cms.js`,
