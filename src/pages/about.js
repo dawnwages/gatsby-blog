b66bb5da9313d7ab3a96ce70d2b148e09ddc394f
Dawn <dawn.wages@gmail.com>
2020-02-01 01:00:09 -0500

    animenmusic

--- src/pages/about.js
+++ src/pages/about.js
@@ -170,8 +170,8 @@ const AboutPage = ({ data }, location) => {
             <a href={"http://www.dawnwages.info/theo"} target={"_blank"}>
               dog, Thelonious "Theo" Ace Hood Monk
             </a>{" "}
-            learning the ukulele, listening to Solange and enjoying black queer
-            femme events around the city.
+            learning the ukulele, listening to music and watching anime, and
+            enjoying black queer femme events around the city.
           </p>
           <p>
             Don't forget to check out the{" "}
---
+++ src/pages/animenmusic.js
@@ -0,0 +1,167 @@
+import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "../utils/normalize.css"
+import "../utils/css/screen.css"
+
+const ElementsPage = ({ data }, location) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout title={siteTitle}>
+      <SEO title="All posts" keywords={[`anime`]} />
+
+      <article className="post-content page-template no-image">
+        <div className="post-content-body">
+          <h1 id="heading-level-1">Music</h1>
+          <hr />
+          <iframe
+            src="https://open.spotify.com/embed/playlist/6NqvEFfTpCgwMCOzvUUBMN"
+            width="300"
+            height="380"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+          <iframe
+            src="https://open.spotify.com/embed/playlist/0V48L8bR34QZvTbnuUxbY3"
+            width="300"
+            height="380"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+          <iframe
+            src="https://open.spotify.com/embed/playlist/77lF04dPPHLr8i9OCeaJMm"
+            width="300"
+            height="380"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+          <iframe
+            src="https://open.spotify.com/embed/playlist/4WqSW8NelSbVv3rEjQlQvX"
+            width="300"
+            height="380"
+            frameborder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          ></iframe>
+
+          <h1 id="heading-level-1">Anime</h1>
+          <hr />
+          <p>
+            Keeping track of the anime with <u>My Anime List</u>{" "}
+            <a href="https://myanimelist.net/profile/ChibiNadia">here</a>. It's
+            not really up to date, but since I'm getting back into it lately
+            (2020) I'd like to do a better job ot tracking what I watch.
+          </p>
+          <h3 id="heading-level-2">Favorite 🔥💓</h3>
+          <p>
+            I love strong female leads with deep back stories and intellectual
+            plots with deamons. Oh the sweet theme of shinigamis coming out to
+            mess with a whole town or snatch a life. I also like cutsie stuff.
+          </p>
+
+          <ul>
+            <li>
+              <a href="https://myanimelist.net/anime/1818/Claymore?q=Claymore">
+                Claymore
+              </a>
+            </li>
+            <li>
+              <a href="https://myanimelist.net/anime/226/Elfen_Lied?q=Elfin%20Lied">
+                Elfin Lied
+              </a>
+            </li>
+            <li>
+              <a href="https://myanimelist.net/anime/121/Fullmetal_Alchemist?q=Full%20Metal%20Alchemist">
+                Full Metal Alchemist
+              </a>
+            </li>
+            <li>
+              <a href="https://myanimelist.net/anime/36884/Hisone_to_Maso-tan?q=Dragon%20Pilot">
+                Dragon Pilot (Hisone to Maso-tan)
+              </a>
+            </li>
+            <li>
+              <a href="https://myanimelist.net/anime/40256/Cannon_Busters">
+                Cannon Busters
+              </a>
+            </li>
+          </ul>
+
+          <h3 id="heading-level-3">Plan To Watch List ⌚</h3>
+          <ul>
+            <li>Demon Slayer</li>
+            <li>Bleach</li>
+            <li>Fire force</li>
+            <li>Bungo Stray Dogs</li>
+            <li>Blood Blockade Battlefront</li>
+            <li>Tokyo Ghoul</li>
+          </ul>
+
+          <h3 id="heading-level-3">Currently Watching ⏳</h3>
+          <p>
+            Right now I'm catching up on what the internet has been on fire
+            about.
+          </p>
+
+          <ul>
+            <li>Hunter x Hunter</li>
+            <li>Sword Art Online s2</li>
+            <li>My Hero Academia </li>
+          </ul>
+        </div>
+      </article>
+    </Layout>
+  )
+}
+
+const indexQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    smallPic: file(
+      relativePath: { eq: "fabio-comparelli-696506-unsplash.jpg" }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 1360) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    medPic: file(relativePath: { eq: "sophia-valkova-30139-unsplash.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1360) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    largePic: file(
+      relativePath: { eq: "vladimir-malyutin-98174-unsplash.jpg" }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 1360) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+export default props => (
+  <StaticQuery
+    query={indexQuery}
+    render={data => (
+      <ElementsPage location={props.location} data={data} {...props} />
+    )}
+  />
+)
